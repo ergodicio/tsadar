Fixed parameter logging bug when m was held constant Updated loss calculations to postporocess with chi squared Fixed plot scaling issue Updated env files to prevent package issues

tsadar/inverse/loss_function.py
@@ -51,7 +51,7 @@ class LossFunction:
         self._vg_func_ = filter_jit(filter_value_and_grad(self.__loss__, has_aux=True))
         ## this will be replaced with jacobian params jacobian inverse
         self._h_func_ = filter_jit(filter_hessian(self._loss_for_hess_fn_))
-        self.array_loss = filter_jit(self.calc_loss)
+        self.array_loss = filter_jit(self.post_loss)
 
     def _get_normed_batch_(self, batch: Dict):
         """
@@ -188,14 +188,11 @@ class LossFunction:
                 (lamAxisE > self.cfg["data"]["fit_rng"]["blue_min"])
                 & (lamAxisE < self.cfg["data"]["fit_rng"]["blue_max"]),
                 _error_,
-                0.0,
+                jnp.nan,
             )
-            # used_points += jnp.sum(
-            #     (lamAxisE > self.cfg["data"]["fit_rng"]["blue_min"])
-            #     & (lamAxisE < self.cfg["data"]["fit_rng"]["blue_max"])
-            # )
+
             e_error += reduce_func(_error_)
-            sqdev["ele"] += _error_
+            sqdev["ele"] += jnp.nan_to_num(_error_)
 
         if self.cfg["other"]["extraoptions"]["fit_EPWr"]:
             _error_ = self.loss_functionals(e_data, ThryE, uncert[1], method=self.cfg["optimizer"]["loss_method"])
@@ -203,19 +200,18 @@ class LossFunction:
                 (lamAxisE > self.cfg["data"]["fit_rng"]["red_min"])
                 & (lamAxisE < self.cfg["data"]["fit_rng"]["red_max"]),
                 _error_,
-                0.0,
+                jnp.nan,
             )
-            # used_points += jnp.sum(
-            #     (lamAxisE > self.cfg["data"]["fit_rng"]["red_min"])
-            #     & (lamAxisE < self.cfg["data"]["fit_rng"]["red_max"])
-            # )
-
+            
             e_error += reduce_func(_error_)
-            sqdev["ele"] += _error_
+            if self.cfg["other"]["extraoptions"]["fit_EPWb"]:
+                #the set e_error to the true mean if both sides are fit
+                e_error*=1./2.
+            sqdev["ele"] += jnp.nan_to_num(_error_)
 
         return i_error, e_error, sqdev
 
-    def calc_loss(self, ts_params, batch: Dict):
+    def calc_loss(self, ts_params, batch: Dict, denom, reduce_func):
         """
         This function calculates the value of the loss function
 
@@ -242,8 +238,8 @@ class LossFunction:
                 lamAxisI,
                 ThryE,
                 lamAxisE,
-                denom=[jnp.square(self.i_norm), jnp.square(self.e_norm)],
-                reduce_func=jnp.mean,
+                denom,
+                reduce_func,
             )
             i_error2, e_error2, sqdev = self.calc_ei_error(
                 batch["b2"],
@@ -251,8 +247,8 @@ class LossFunction:
                 lamAxisI,
                 ThryE_rot,
                 lamAxisE,
-                denom=[jnp.square(self.i_norm), jnp.square(self.e_norm)],
-                reduce_func=jnp.mean,
+                denom,
+                reduce_func,
             )
             i_error = i_error1 + i_error2
             e_error = e_error1 + e_error2
@@ -260,15 +256,16 @@ class LossFunction:
             normed_batch = self._get_normed_batch_(batch["b1"])
         else:
             ThryE, ThryI, lamAxisE, lamAxisI = self.ts_diag(ts_params, batch)
-
+            if denom == []:
+                denom = [ThryI, ThryE]
             i_error, e_error, sqdev = self.calc_ei_error(
                 batch,
                 ThryI,
                 lamAxisI,
                 ThryE,
                 lamAxisE,
-                uncert=[jnp.square(self.i_norm), jnp.square(self.e_norm)],
-                reduce_func=jnp.mean,
+                denom,
+                reduce_func,
             )
 
             normed_batch = self._get_normed_batch_(batch)
@@ -307,8 +304,19 @@ class LossFunction:
         """
 
         weights = eqx.combine(static_weights, diff_weights)
-        total_loss, sqdev, ThryE, normed_e_data, params = self.calc_loss(weights, batch)
+        total_loss, sqdev, ThryE, normed_e_data, params = self.calc_loss(
+            weights, batch, denom=[jnp.square(self.i_norm), jnp.square(self.e_norm)], reduce_func=jnp.nanmean)
         return total_loss, [ThryE, params]
+    
+    def post_loss(self, weights, batch: Dict):
+        """
+        Output wrapper for postprocessing
+        """
+        #self.array_loss = filter_jit(self.calc_loss)
+        
+        total_loss, sqdev, ThryE, normed_e_data, params = self.calc_loss(
+            weights, batch, denom=[], reduce_func=jnp.nanmean)
+        return total_loss, sqdev, ThryE, normed_e_data, params
 
     def loss_functionals(self, d, t, uncert, method="l2"):
         """

tsadar/utils/plotting/plotters.py
@@ -119,8 +119,7 @@ def plot_loss_hist(config, losses_init, losses, reduced_points, td):
     chi-squared is used and the reduced metric is chi-squared per degree of freedom but this will not necessarily be
     near 1 since Thomson scattering often does not conform to chi-squared statistics.
 
-    Known issues:
-        The pre-refitting values are currently not being imported so the final losses are just plotted twice.
+    With the update of losses to loss/point this may be reduced to just "reduced losses"
 
     Args:
         config: configuration dictionary created from the input decks
@@ -151,6 +150,7 @@ def plot_loss_hist(config, losses_init, losses, reduced_points, td):
     ax[0].set_ylabel("Counts")
     ax[0].set_title("Normalized $L^2$ Norm of the Error")
     ax[0].grid()
+    ax[0].legend(["Pre-refit Losses", "Post-refit Losses"])
     ax[1].hist([losses_init, losses], 40)
     # ax[1].hist(losses, 128)
     ax[1].set_yscale("log")
@@ -750,10 +750,10 @@ def detailed_lineouts(config, all_data, all_axes, fits, losses, red_losses, sqde
         fig, ax = plt.subplots(2, 2, figsize=(12, 8), squeeze=False, tight_layout=True, sharex=True)
     
         if config["other"]["extraoptions"]["load_ele_spec"]:
-            #s_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_start"]))
-            #e_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_end"]))
+            s_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_start"]))
+            e_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_end"]))
             ax[0][0].plot(
-                all_axes["epw_y"], np.squeeze(all_data["e_data"][loss_inds[i]]), label="Data"
+                all_axes["epw_y"][s_ind:e_ind], np.squeeze(all_data["e_data"][loss_inds[i]][s_ind:e_ind]), label="Data"
             )
             ax[0][0].plot(
                 all_axes["epw_y"], np.squeeze(fits["ele"]["total_spec"][loss_inds[i]]), label="Total Fit"
@@ -779,6 +779,10 @@ def detailed_lineouts(config, all_data, all_axes, fits, losses, red_losses, sqde
             ax[0][0].legend(bbox_to_anchor = (0.8, 1.05), fontsize=12)
             ax[0][0].grid()
             ax[0][0].set_xlim([config["plotting"]["ele_window_start"], config["plotting"]["ele_window_end"]])
+            #ax[0][0].autoscale()
+            # ax[0][0].set_ylim(
+            #     [None if config["plotting"]["data_cbar_l"] == "data" else config["plotting"]["data_cbar_l"],
+            #     None if config["plotting"]["data_cbar_u"] == "data" else config["plotting"]["data_cbar_u"]])
 
             ax[1][0].plot(
                 all_axes["epw_y"], np.squeeze(sqdevs["ele"][loss_inds[i]]), label="Residual"
@@ -841,10 +845,10 @@ def detailed_lineouts(config, all_data, all_axes, fits, losses, red_losses, sqde
         fig, ax = plt.subplots(2, 2, figsize=(12, 8), squeeze=False, tight_layout=True, sharex=True)
     
         if config["other"]["extraoptions"]["load_ele_spec"]:
-            #s_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_start"]))
-            #e_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_end"]))
+            s_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_start"]))
+            e_ind = np.argmin(np.abs(all_axes["epw_y"] - config["plotting"]["ele_window_end"]))
             ax[0][0].plot(
-                all_axes["epw_y"], np.squeeze(all_data["e_data"][loss_inds[-1-i]]), label="Data"
+                all_axes["epw_y"][s_ind:e_ind], np.squeeze(all_data["e_data"][loss_inds[-1-i]][s_ind:e_ind]), label="Data"
             )
             ax[0][0].plot(
                 all_axes["epw_y"], np.squeeze(fits["ele"]["total_spec"][loss_inds[-1-i]]), label="Total Fit"
@@ -870,7 +874,10 @@ def detailed_lineouts(config, all_data, all_axes, fits, losses, red_losses, sqde
             ax[0][0].legend(bbox_to_anchor = (0.8, 1.05), fontsize=12)
             ax[0][0].grid()
             ax[0][0].set_xlim([config["plotting"]["ele_window_start"], config["plotting"]["ele_window_end"]])
-
+            # ax[0][0].set_ylim(
+            #     [None if config["plotting"]["data_cbar_l"] == "data" else config["plotting"]["data_cbar_l"],
+            #     None if config["plotting"]["data_cbar_u"] == "data" else config["plotting"]["data_cbar_u"]])
+            #ax[0][0].autoscale()
             ax[1][0].plot(
                 all_axes["epw_y"], np.squeeze(sqdevs["ele"][loss_inds[-1-i]]), label="Residual"
             )