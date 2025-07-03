corrected the sign in the imaginary electron susceptibility

tsadar/core/physics/form_factor.py
@@ -258,7 +258,7 @@ class FormFactor:
         df = jnp.diff(fe_vphi, 1, 1) / jnp.diff(xie, 1, 1)
         df = jnp.append(df, jnp.zeros((len(ne), 1, len(self.scattering_angles["sa"]))), 1)
 
-        chiEI = jnp.pi / (klde**2) * 1j * df
+        chiEI = -jnp.pi / (klde**2) * 1j * df
         
         ratmod = jnp.exp(interp1d(self.xi1, vx, jnp.log(fe), extrap=[-50, -50]))
         ratdf = jnp.gradient(ratmod, self.xi1[1] - self.xi1[0])